573026108841663f5a463a5e2ec22b1227558963
Mauricio Blum <mauriciocblum@gmail.com>
2019-08-16 17:59:30 -0300

    Mock calls and fakers

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,18 +1,28 @@
 const request = require("supertest");
+const nodemailer = require("nodemailer");
+
 const app = require("../../src/app");
 const truncate = require("../utils/truncate");
-const { User } = require("../../src/app/models");
+const factory = require("../factories");
+
+jest.mock("nodemailer");
+
+const transport = {
+  sendMail: jest.fn()
+};
 
 describe("Authentication", () => {
   beforeEach(async () => {
     await truncate();
   });
 
+  beforeAll(() => {
+    nodemailer.createTransport.mockReturnValue(transport);
+  });
+
   it("should be able to authenticate with valid credentials", async () => {
-    const user = await User.create({
-      name: "Mau",
-      email: "mau@mau.com",
-      password_hash: "123"
+    const user = await factory.create("User", {
+      password: "123"
     });
 
     const response = await request(app)
@@ -20,5 +30,68 @@ describe("Authentication", () => {
       .send({ email: user.email, password: "123" });
 
     expect(response.status).toBe(200);
+  });
+
+  it("should not be able to authenticate with invalid credentials", async () => {
+    const user = await factory.create("User", {
+      password: "12356456"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({ email: user.email, password: "123456" });
+
+    expect(response.status).toBe(401);
+  });
+
+  it("should return jwt token when authenticated", async () => {
+    const user = await factory.create("User", {
+      password: "123"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({ email: user.email, password: "123" });
+
+    expect(response.body).toHaveProperty("token");
+  });
+
+  it("should be able to access private routes when authenticated", async () => {
+    const user = await factory.create("User");
+
+    const response = await request(app)
+      .get("/dashboard")
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should be not be able to access private routes when not authenticated", async () => {
+    const response = await request(app).get("/dashboard");
+
+    expect(response.status).toBe(401);
+  });
+
+  it("should be not be able to access private token is invalid", async () => {
+    const response = await request(app)
+      .get("/dashboard")
+      .set("Authorization", "Bearer 123123");
+
+    expect(response.status).toBe(401);
+  });
+
+  it("should receive email notification when authenticated", async () => {
+    const user = await factory.create("User", {
+      password: "123"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({ email: user.email, password: "123" });
+
+    expect(transport.sendMail).toHaveBeenCalledTimes(1);
+    expect(transport.sendMail.mock.calls[0][0].to).toBe(
+      `${user.name} <${user.email}>`
+    );
   });
 });
---
+++ src/config/mail.js
@@ -0,0 +1,8 @@
+module.exports = {
+  auth: {
+    user: "asdadad",
+    pass: "s233d3"
+  },
+  port: 2525,
+  host: "smtp.mailtrap.io"
+};
